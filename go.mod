0ca1a714f17302607423ef06113b45f6c6d5e170
bojan <bojanradovski@gmail.com>
2024-07-21 19:03:19 +0200

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Bokizz/temporary
 
 go 1.22.5
+
+require github.com/Bokizz/temporary2 v0.0.0-20240721165825-b7e0e39d923b // indirect
--- temporary.go
+++ temporary.go
@@ -1,9 +1,23 @@
 package temporary
 
+import "github.com/Bokizz/temporary2"
+
 func Hey() string {
 	return "Hey,hey!"
 }
 
 func Listen() string {
 	return "Listen,hey!"
+}
+func Heardnon() string {
+	return "Nothing of importance :)"
+}
+func Heardsom() string {
+	return "They are taking the hobbits to Isengard"
+}
+func Listenednon() string {
+	return temporary2.WhenListen(Heardnon())
+}
+func Listenedsom() string {
+	return temporary2.WhenListen(Heardsom())
 }
